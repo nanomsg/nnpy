0d2b6a07a619dc6dc32f8b09bcc25ef7ce15a43a
Rizo Isrof <rizo@odis.io>
2014-01-15 13:22:23 +0000

    Idiomatic adjustments.

--- nnpy/socket.py
+++ nnpy/socket.py
@@ -35,22 +35,19 @@ class Socket(object):
 		assert rc >= 0, rc
 	
 	def bind(self, addr):
-		if (isinstance(addr, str)):
-			addr = addr.encode()
+		addr = addr.encode() if (isinstance(addr, str)) else addr
 		buf = ffi.new('char[]', addr)
 		rc = nanomsg.nn_bind(self.sock, buf)
 		assert rc > 0, rc
 	
 	def connect(self, addr):
-		if (isinstance(addr, str)):
-			addr = addr.encode()
+		addr = addr.encode() if (isinstance(addr, str)) else addr
 		buf = ffi.new('char[]', addr)
 		rc = nanomsg.nn_connect(self.sock, buf)
 		assert rc > 0, rc
 	
 	def send(self, data, flags=0):
-		if (isinstance(data, str)):
-			data = data.encode()
+		data = data.encode() if (isinstance(data, str)) else data
 		l = len(data)
 		buf = ffi.new('char[%i]' % l, data)
 		rc = nanomsg.nn_send(self.sock, buf, l, flags)
