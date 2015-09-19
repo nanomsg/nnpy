5c8a8e7461836bdf2b2da5d056f98c86b88914a8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2015-09-19 12:16:31 +0200

    Convert tabs to spaces for PEP 8 compliance

--- generate.py
+++ generate.py
@@ -5,59 +5,59 @@ INCLUDE = ['/usr/include/nanomsg', '/usr/local/include/nanomsg']
 BLOCKS = {'{': '}', '(': ')'}
 
 def functions():
-	
-	for dir in INCLUDE:
-		if os.path.exists(dir):
-			break
-	
-	lines = []
-	for fn in os.listdir(dir):
-		with open(os.path.join(dir, fn)) as f:
-			cont = ''
-			for ln in f:
-				
-				if cont == ',':
-					lines.append(ln)
-					cont = ''
-				if cont in BLOCKS:
-					lines.append(ln)
-					if BLOCKS[cont] in ln:
-						cont = ''
-				if not (ln.startswith('NN_EXPORT')
-					or ln.startswith('typedef')):
-					continue
-				
-				lines.append(ln)
-				cont = ln.strip()[-1]
-	
-	return ''.join(ln[10:] if ln.startswith('NN_') else ln for ln in lines)
+    
+    for dir in INCLUDE:
+        if os.path.exists(dir):
+            break
+    
+    lines = []
+    for fn in os.listdir(dir):
+        with open(os.path.join(dir, fn)) as f:
+            cont = ''
+            for ln in f:
+                
+                if cont == ',':
+                    lines.append(ln)
+                    cont = ''
+                if cont in BLOCKS:
+                    lines.append(ln)
+                    if BLOCKS[cont] in ln:
+                        cont = ''
+                if not (ln.startswith('NN_EXPORT')
+                    or ln.startswith('typedef')):
+                    continue
+                
+                lines.append(ln)
+                cont = ln.strip()[-1]
+    
+    return ''.join(ln[10:] if ln.startswith('NN_') else ln for ln in lines)
 
 def symbols(headers):
-	
-	ffi = FFI()
-	ffi.cdef(headers)
-	nanomsg = ffi.dlopen('nanomsg')
-	
-	lines = []
-	for i in range(1024):
-		
-		val = ffi.new('int*')
-		name = nanomsg.nn_symbol(i, val)
-		if name == ffi.NULL:
-			break
-		
-		name = ffi.string(name).decode()
-		name = name[3:] if name.startswith('NN_') else name
-		lines.append('%s = %s' % (name, val[0]))
-	
-	return '\n'.join(lines) + '\n'
+    
+    ffi = FFI()
+    ffi.cdef(headers)
+    nanomsg = ffi.dlopen('nanomsg')
+    
+    lines = []
+    for i in range(1024):
+        
+        val = ffi.new('int*')
+        name = nanomsg.nn_symbol(i, val)
+        if name == ffi.NULL:
+            break
+        
+        name = ffi.string(name).decode()
+        name = name[3:] if name.startswith('NN_') else name
+        lines.append('%s = %s' % (name, val[0]))
+    
+    return '\n'.join(lines) + '\n'
 
 def run():
-	headers = functions()
-	with open('nnpy/nanomsg.h', 'w') as f:
-		f.write(headers)
-	with open('nnpy/constants.py', 'w') as f:
-		f.write(symbols(headers))
+    headers = functions()
+    with open('nnpy/nanomsg.h', 'w') as f:
+        f.write(headers)
+    with open('nnpy/constants.py', 'w') as f:
+        f.write(symbols(headers))
 
 if __name__ == '__main__':
-	run()
+    run()
--- nnpy/socket.py
+++ nnpy/socket.py
@@ -5,60 +5,60 @@ import sys
 NN_MSG = int(ffi.cast("size_t", -1))
 
 class Socket(object):
-	
-	def __init__(self, domain, protocol):
-		self.sock = nanomsg.nn_socket(domain, protocol)
-	
-	def close(self):
-		nanomsg.nn_close(self.sock)
-	
-	def getsockopt(self, level, option):
-		buf = ffi.new('int*')
-		size = ffi.new('size_t*')
-		size[0] = 4
-		rc = nanomsg.nn_getsockopt(self.sock, level, option, buf, size)
-		assert rc >= 0, rc
-		return buf[0]
-	
-	def setsockopt(self, level, option, value):
-		if isinstance(value, int):
-			buf = ffi.new('int*')
-			buf[0] = value
-			vlen = 4
-		elif isinstance(value, str):
-			buf = ffi.new('char[%s]' % len(value), value.encode())
-			vlen = len(value)
-		elif isinstance(value, bytes):
-			buf = ffi.new('char[%s]' % len(value), value)
-			vlen = len(value)
-		else:
-			raise TypeError("value must be a int, str or bytes")
-		rc = nanomsg.nn_setsockopt(self.sock, level, option, buf, vlen)
-		assert rc >= 0, rc
-	
-	def bind(self, addr):
-		addr = addr.encode() if isinstance(addr, str) else addr
-		buf = ffi.new('char[]', addr)
-		rc = nanomsg.nn_bind(self.sock, buf)
-		assert rc > 0, rc
-	
-	def connect(self, addr):
-		addr = addr.encode() if isinstance(addr, str) else addr
-		buf = ffi.new('char[]', addr)
-		rc = nanomsg.nn_connect(self.sock, buf)
-		assert rc > 0, rc
-	
-	def send(self, data, flags=0):
-		data = data.encode() if isinstance(data, str) else data
-		l = len(data)
-		buf = ffi.new('char[%i]' % l, data)
-		rc = nanomsg.nn_send(self.sock, buf, l, flags)
-		assert rc > 0, rc
-	
-	def recv(self, flags=0):
-		buf = ffi.new('char**')
-		rc = nanomsg.nn_recv(self.sock, buf, NN_MSG, flags)
-		assert rc > 0, rc
-		s = ffi.buffer(buf[0], rc)[:]
-		nanomsg.nn_freemsg(buf[0])
-		return s
+    
+    def __init__(self, domain, protocol):
+        self.sock = nanomsg.nn_socket(domain, protocol)
+    
+    def close(self):
+        nanomsg.nn_close(self.sock)
+    
+    def getsockopt(self, level, option):
+        buf = ffi.new('int*')
+        size = ffi.new('size_t*')
+        size[0] = 4
+        rc = nanomsg.nn_getsockopt(self.sock, level, option, buf, size)
+        assert rc >= 0, rc
+        return buf[0]
+    
+    def setsockopt(self, level, option, value):
+        if isinstance(value, int):
+            buf = ffi.new('int*')
+            buf[0] = value
+            vlen = 4
+        elif isinstance(value, str):
+            buf = ffi.new('char[%s]' % len(value), value.encode())
+            vlen = len(value)
+        elif isinstance(value, bytes):
+            buf = ffi.new('char[%s]' % len(value), value)
+            vlen = len(value)
+        else:
+            raise TypeError("value must be a int, str or bytes")
+        rc = nanomsg.nn_setsockopt(self.sock, level, option, buf, vlen)
+        assert rc >= 0, rc
+    
+    def bind(self, addr):
+        addr = addr.encode() if isinstance(addr, str) else addr
+        buf = ffi.new('char[]', addr)
+        rc = nanomsg.nn_bind(self.sock, buf)
+        assert rc > 0, rc
+    
+    def connect(self, addr):
+        addr = addr.encode() if isinstance(addr, str) else addr
+        buf = ffi.new('char[]', addr)
+        rc = nanomsg.nn_connect(self.sock, buf)
+        assert rc > 0, rc
+    
+    def send(self, data, flags=0):
+        data = data.encode() if isinstance(data, str) else data
+        l = len(data)
+        buf = ffi.new('char[%i]' % l, data)
+        rc = nanomsg.nn_send(self.sock, buf, l, flags)
+        assert rc > 0, rc
+    
+    def recv(self, flags=0):
+        buf = ffi.new('char**')
+        rc = nanomsg.nn_recv(self.sock, buf, NN_MSG, flags)
+        assert rc > 0, rc
+        s = ffi.buffer(buf[0], rc)[:]
+        nanomsg.nn_freemsg(buf[0])
+        return s
