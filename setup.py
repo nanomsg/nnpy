96a0175f897621b8e828293c6e3ba089eb50ba4e
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-07-29 20:39:28 +0200

    Bump version number to 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nnpy',
-    version='1.1',
+    version='1.2',
     url='https://github.com/nanomsg/nnpy',
     license='MIT',
     author='Dirkjan Ochtman',
