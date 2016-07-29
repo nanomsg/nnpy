656d2ace1940d8f45d8f52cc2b2fbbad4279e9c0
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-07-29 16:17:23 +0200

    Raise descriptive Exception on errors

--- nnpy/socket.py
+++ nnpy/socket.py
@@ -5,10 +5,21 @@ NN_MSG = int(ffi.cast("size_t", -1))
 
 ustr = str if sys.version_info[0] > 2 else unicode
 
+class NNError(Exception):
+    pass
+
 class Socket(object):
     
     def __init__(self, domain, protocol):
         self.sock = nanomsg.nn_socket(domain, protocol)
+    
+    def _error(self, rc, value=None):
+        if rc < 0:
+            chars = nanomsg.nn_strerror(nanomsg.nn_errno())
+            raise NNError(ffi.string(chars))
+        if callable(value):
+            return value()
+        return value
     
     def close(self):
         nanomsg.nn_close(self.sock)
@@ -18,8 +29,7 @@ class Socket(object):
         size = ffi.new('size_t*')
         size[0] = 4
         rc = nanomsg.nn_getsockopt(self.sock, level, option, buf, size)
-        assert rc >= 0, rc
-        return buf[0]
+        return self._error(rc, lambda: buf[0])
     
     def setsockopt(self, level, option, value):
         if isinstance(value, int):
@@ -35,19 +45,19 @@ class Socket(object):
         else:
             raise TypeError("value must be a int, str or bytes")
         rc = nanomsg.nn_setsockopt(self.sock, level, option, buf, vlen)
-        assert rc >= 0, rc
+        self._error(rc)
     
     def bind(self, addr):
         addr = addr.encode() if isinstance(addr, ustr) else addr
         buf = ffi.new('char[]', addr)
         rc = nanomsg.nn_bind(self.sock, buf)
-        assert rc > 0, rc
+        return self._error(rc, rc)
     
     def connect(self, addr):
         addr = addr.encode() if isinstance(addr, ustr) else addr
         buf = ffi.new('char[]', addr)
         rc = nanomsg.nn_connect(self.sock, buf)
-        assert rc > 0, rc
+        return self._error(rc, rc)
     
     def send(self, data, flags=0):
         if isinstance(data, memoryview):
@@ -56,12 +66,12 @@ class Socket(object):
             data = data.encode() if isinstance(data, ustr) else data
             buf = ffi.new('char[%i]' % len(data), data)
         rc = nanomsg.nn_send(self.sock, buf, len(data), flags)
-        assert rc > 0, rc
+        return self._error(rc, rc)
     
     def recv(self, flags=0):
         buf = ffi.new('char**')
         rc = nanomsg.nn_recv(self.sock, buf, NN_MSG, flags)
-        assert rc > 0, rc
+        self._error(rc)
         s = ffi.buffer(buf[0], rc)[:]
         nanomsg.nn_freemsg(buf[0])
         return s
