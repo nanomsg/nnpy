07fcb983fb08c73a01e1d7348c2c92906bca98f3
wrobell <wrobell@pld-linux.org>
2016-09-09 17:26:05 +0100

    Add result code and errno values to the `NNError` exception class when raising error due to `nanomsg` function error.

--- nnpy/errors.py
+++ nnpy/errors.py
@@ -1,12 +1,16 @@
 from _nnpy import ffi, lib as nanomsg
 
 class NNError(Exception):
-    pass
+    def __init__(self, result_code, error_no, *args, **kwargs):
+        self.result_code = result_code
+        self.error_no = error_no
 
 def convert(rc, value=None):
     if rc < 0:
-        chars = nanomsg.nn_strerror(nanomsg.nn_errno())
-        raise NNError(ffi.string(chars).decode())
+        error_no = nanomsg.nn_errno()
+        chars = nanomsg.nn_strerror(error_no)
+        msg = ffi.string(chars).decode()
+        raise NNError(rc, error_no, msg)
     if callable(value):
         return value()
     return value
