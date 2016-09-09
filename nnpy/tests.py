901958c42c366e40f843d583d950a4beb4b6207b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-09-09 15:25:30 +0200

    Add some more test coverage

--- nnpy/tests.py
+++ nnpy/tests.py
@@ -5,11 +5,12 @@ class Tests(unittest.TestCase):
     def test_basic(self):
 
         pub = nnpy.Socket(nnpy.AF_SP, nnpy.PUB)
+        pub.setsockopt(nnpy.SOL_SOCKET, nnpy.IPV4ONLY, 0)
         pub.bind('inproc://foo')
         self.assertEqual(pub.getsockopt(nnpy.SOL_SOCKET, nnpy.DOMAIN), 1)
 
         sub = nnpy.Socket(nnpy.AF_SP, nnpy.SUB)
-        sub.connect('inproc://foo')
+        sub_conn = sub.connect('inproc://foo')
         sub.setsockopt(nnpy.SUB, nnpy.SUB_SUBSCRIBE, '')
 
         pub.send('FLUB')
@@ -17,6 +18,8 @@ class Tests(unittest.TestCase):
         self.assertEqual(poller.poll(), 1)
         self.assertEqual(sub.recv(), 'FLUB')
         self.assertEqual(pub.get_statistic(nnpy.STAT_MESSAGES_SENT), 1)
+        pub.close()
+        sub.shutdown(sub_conn)
 
 def suite():
     return unittest.makeSuite(Tests)
