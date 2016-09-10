d16a21decb365d94df2b4fb36f99bac68903d31b
wrobell <wrobell@pld-linux.org>
2016-09-10 11:44:23 +0100

    Use `NNError` super class constructor properly. Removed `NNError.result_code` attribute as it is probably always -1.

--- nnpy/errors.py
+++ nnpy/errors.py
@@ -1,8 +1,8 @@
 from _nnpy import ffi, lib as nanomsg
 
 class NNError(Exception):
-    def __init__(self, result_code, error_no, *args, **kwargs):
-        self.result_code = result_code
+    def __init__(self, error_no, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.error_no = error_no
 
 def convert(rc, value=None):
